b6660791a2fca8a235a5d8fe4d3a850766254e78
veapon <veapon88@gmail.com>
2015-02-28 18:35:02 +0800

    update

---
+++ resources/views/test.blade.php
@@ -0,0 +1,11 @@
+@extends('layouts.base')
+
+@section('sidebar')
+    @parent
+
+    <p>This is appended to the master sidebar.</p>
+@stop
+
+@section('content')
+    <p>This is my body content.</p>
+@stop
