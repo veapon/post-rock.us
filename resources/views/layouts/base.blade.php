5c35406aa15524a8864fc39ee583331d034eb3a9
veapon <veapon88@gmail.com>
2015-03-01 18:48:25 +0800

    update

--- resources/views/layouts/base.blade.php
+++ resources/views/layouts/base.blade.php
@@ -1,16 +1,16 @@
 <!DOCTYPE html>
 <html lang="en">
 <head>
-	<meta charset="UTF-8">
 	<title>Document</title>
+	<meta charset="UTF-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<link rel="stylesheet" href="css/semantic.min.css">
+	<link href="//maxcdn.bootstrapcdn.com/font-awesome/4.2.0/css/font-awesome.min.css" rel="stylesheet">
+	<link rel="stylesheet" href="css/pr.css">
+	<script src="http://cdn.staticfile.org/jquery/2.1.1-rc2/jquery.min.js"></script>
+	<script src="js/semantic.min.js"></script>
 </head>
 <body>
-        @section('sidebar')
-            This is the master sidebar.
-        @show
-
-        <div class="container">
-            @yield('content')
-        </div>
+	<div class="ui left sidebar menu"></div>
 </body>
 </html>
